350995cdb30daf56a54b3e0233e82e2102e5b6ac
dea asti anandita <54872316+DeaAnandita@users.noreply.github.com>
2026-01-13 22:10:52 +0700

    remove darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: false, // 🔥 MATIKAN DARK MODE
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
